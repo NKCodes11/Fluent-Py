e96757d77b51d6433b009506521981e33b1c129d
Nathaniel <75357893+NKCodes11@users.noreply.github.com>
2023-07-23 23:17:37 -0400

    motorcycle

--- FLUENT PY CH3/motorcycles.py
+++ FLUENT PY CH3/motorcycles.py
@@ -13,10 +13,17 @@ motorcycles.insert(2,"ferrari")
 print(motorcycles)
 
 #you can delete elements by using delete or pop. pop will delete the last one and del will delete which ever is index.
+print("\nyou can delete elements by using delete or pop. pop will delete the last one and del will delete which ever is index.)")
 popmotor = motorcycles.pop()
 del motorcycles[0]
 
-print(popmotor)
+print("\nthis is the popped item from the end -->"+popmotor)
 print(motorcycles)
 # you can also pop by position as well
 
+last_owned = motorcycles.pop()
+print("\nThe last motorcycle I owned was a " + last_owned.title() + ".\n")
+first_owned = motorcycles.pop(0)
+print("first owned motorcycle I owned was a "+ first_owned.title()+ ".\n")
+#popped items are no longer in the list
+print("items that are popped are no longer in the list")
