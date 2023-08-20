cc4ea505df60d644518a32340b21f51af621fcfe
Nathaniel <75357893+NKCodes11@users.noreply.github.com>
2023-08-20 19:09:36 -0400

    push

--- FLUENT PYYY CH 5/toppings.py
+++ FLUENT PYYY CH 5/toppings.py
@@ -25,4 +25,4 @@ for requested_topping in requested_toppings:
         print("Sorry, we don't have " + requested_topping + ".")
 
 
-print("\nFinished making your pizza!")
+print("\nFinished making your pizzas!")
